b102b295f1021fdc93b627afb293b613fe488098
YangJongYeol <yajya004@gmail.com>
2018-02-04 02:23:45 +0900

    Add 1008

---
+++ 1008/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double num1;
+        double num2;
+
+        num1 = scanner.nextDouble();
+        num2 = scanner.nextDouble();
+
+        System.out.println(num1/num2);
+    }
+}
