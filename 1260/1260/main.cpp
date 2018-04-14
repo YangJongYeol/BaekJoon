a6f961dd007cd1c3a3e2c93b3b921329cd213a13
YangJongYeol <yajya004@gmail.com>
2018-04-14 23:17:35 +0900

    Add solutions

---
+++ 1260/1260/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  1260
+//
+//  Created by 양종열 on 2018. 3. 21..
+//  Copyright © 2018년 양종열. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    ios::sync_with_stdio(false);
+    
+    int N, M, V;
+    
+
+    
+    return 0;
+}
